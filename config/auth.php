bc94b2236bb617a7da11489cf87f54b0f56ea1ad
ahamed-kashif <ahamed.kashif@northsouth.edu>
2024-12-15 01:53:58 +0600

    admin login resolved

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => env('AUTH_GUARD', 'admin'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'admin' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ]
     ],
 
     /*
@@ -63,7 +67,7 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
+        ]
 
         // 'users' => [
         //     'driver' => 'database',
@@ -96,7 +100,7 @@ return [
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
-        ],
+        ]
     ],
 
     /*
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('admin')->group(function () {
+    Route::post('/login',[\App\Http\Controllers\Auth\Api\Auth\AdminLoginController::class,'login']);
+    Route::post('/get-access-token',[\App\Http\Controllers\Auth\Api\Auth\AdminLoginController::class,'refresh']);
+    Route::get('/profile',function (Request $request) {
+        return \Illuminate\Support\Facades\Auth::guard('admin')->user();
+    })->middleware(['auth:admin','abilities:'.\App\Enums\TokenAbility::ACCESS_API->value]);
+});
+
+
