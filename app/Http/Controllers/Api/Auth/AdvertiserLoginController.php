daa1e2c6c8f7942f5220cd1df6310ab697e631b5
rashu-pro <rashu.pro@gmail.com>
2024-12-17 12:49:09 +0600

    advertiser login api completed

---
+++ app/Http/Controllers/Api/Auth/AdvertiserLoginController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Enums\TokenAbility;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\AdminLoginRequest;
+use App\Http\Requests\Auth\AdvertiserLoginRequest;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Advertiser;
+use App\Models\User;
+use App\Traits\ApiResponse;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class AdvertiserLoginController extends Controller
+{
+    use ApiResponse;
+    public function login(AdvertiserLoginRequest $request)
+    {
+        try {
+            $request->authenticate();
+            $user = $request->user;
+            return $this->createTokens($user);
+        } catch (ValidationException $e) {
+            return $this->errorResponse($e->getMessage(), $e->errors(), 422);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e->getMessage());
+        }
+    }
+
+    public function refresh(Request $request)
+    {
+        try {
+            $refreshToken = $request->bearerToken();
+
+            if (!$refreshToken) {
+                throw ValidationException::withMessages([
+                    'refresh_token' => ['Refresh token is required.'],
+                ]);
+            }
+            $token = PersonalAccessToken::findToken($refreshToken);
+
+            if (!$token || !$token->can(TokenAbility::ISSUE_ACCESS_TOKEN->value) || $token->expires_at->isPast()) {
+                throw ValidationException::withMessages([
+                    'refresh_token' => ['The refresh token is invalid or expired.'],
+                ]);
+            }
+
+            $user = $token->tokenable;
+            $token->delete();
+
+            return $this->createTokens($user);
+        } catch (ValidationException $e) {
+            return $this->errorResponse($e->getMessage(), $e->errors(), 422);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e->getMessage());
+        }
+    }
+
+    protected function createTokens(Advertiser $user)
+    {
+        $accessTokenExpiresAt = Carbon::now()->addMinutes(config('sanctum.ac_expiration'));
+        $refreshTokenExpiresAt = Carbon::now()->addMinutes(config('sanctum.rt_expiration'));
+
+        $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value], $accessTokenExpiresAt)->plainTextToken;
+        $refreshToken = $user->createToken('refresh_token', [TokenAbility::ISSUE_ACCESS_TOKEN->value], $refreshTokenExpiresAt)->plainTextToken;
+
+        return $this->successResponse('Logged in successfully.', [
+            'access_token' => $accessToken,
+            'access_token_expires_at' => $accessTokenExpiresAt,
+            'refresh_token' => $refreshToken,
+            'refresh_token_expires_at' => $refreshTokenExpiresAt,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
---
+++ app/Http/Requests/Auth/AdvertiserLoginRequest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Models\Advertiser;
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+
+class AdvertiserLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return !$this->bearerToken();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email','exists:users,email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function authenticate(): void
+    {
+        $this->ensureIsNotRateLimited();
+
+        $user = Advertiser::where('email', $this->email)->first();
+
+        if (!$user || !Hash::check($this->password, $user->password)) {
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        $this->merge(['user' => $user]);
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    /**
+     * Ensure the login request is not rate limited.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+
+    public function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->input('email')).'|'.$this->ip());
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation Error',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+
+    protected function failedAuthorization(): void
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Unauthorized',
+        ], 403));
+    }
+}
