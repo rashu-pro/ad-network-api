a941ddc7ec366bfb708b138b6b1415ace2b271e3
rashu-pro <rashu.pro@gmail.com>
2024-12-31 00:20:45 +0600

    staging and production server parameter changed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,8 +8,8 @@ use OpenApi\Attributes as OA;
 #[
     OA\Info(version: "1.0.0", description: "petshop api", title: "Petshop-api Documentation"),
     OA\Server(url: 'http://localhost:8000', description: "local server"),
-    OA\Server(url: 'http://staging.example.com', description: "staging server"),
-    OA\Server(url: 'http://example.com', description: "production server"),
+    OA\Server(url: 'https://adnetwork.masjidapps.net', description: "staging server"),
+    OA\Server(url: 'https://adnetwork.masjidapps.net', description: "production server"),
     OA\SecurityScheme( securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header", scheme: "bearer"),
 ]
 abstract class Controller
