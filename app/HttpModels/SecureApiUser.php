228e5189aaa6881d0b221e26d314c3737913aa72
ahamed-kashif <ahamed.kashif@northsouth.edu>
2025-01-29 17:46:36 +0600

    business category issue fixed

--- app/HttpModels/SecureApiUser.php
+++ app/HttpModels/SecureApiUser.php
@@ -78,8 +78,8 @@ class SecureApiUser implements HttpModel
         // Formatting the validated data
         return [
             "secure_api_id" => $data["secure_api_id"] ?? null,
-            "isAdPublisher" => !$data["isAdPublisher"],
-            "isAdvertiser" => $data["isAdvertiser"],
+            "isAdPublisher" => $data["isAdPublisher"] ?? $data['businessCategory'] == CompanyCategory::PUBLISHER,
+            "isAdvertiser" => $data["isAdvertiser"] ?? $data['businessCategory'] == CompanyCategory::ADVERTISER,
             "businessName" => $data["business_name"] ?? "",
             "businessInfo" => [
                 "websiteUrl" => $data["advertiser_website"] ?? "",
