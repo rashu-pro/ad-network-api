c4fa131449f328cd92891ee8fd5bb60f8c4ad900
ahamed-kashif <ahamed.kashif@northsouth.edu>
2025-02-09 19:49:50 +0600

    upload banner debug

--- app/Http/Controllers/Api/AdvertiserOptController.php
+++ app/Http/Controllers/Api/AdvertiserOptController.php
@@ -514,8 +514,10 @@ class AdvertiserOptController extends Controller
         }
         Storage::delete('app/private/'.$tempPath);
         return $this->successResponse(message: 'uploaded successfully', data: [
-            'url' => $mappings->map(function ($mapping) {
-                return  $mapping->hasMedia('banner') ? $mapping->getFirstMedia('banner')->getUrl() : '';
+            'url' => $mappings->filter(function ($mapping){
+                return $mapping->hasMedia('banner');
+            })->map(function ($mapping) {
+                return  $mapping->getFirstMedia('banner')->getUrl();
             })->toArray()
         ]);
     }
