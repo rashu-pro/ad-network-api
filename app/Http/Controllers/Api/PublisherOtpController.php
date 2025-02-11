5f6064fefe6188daba66deb761ddd040943475fb
ahamed-kashif <ahamed.kashif@northsouth.edu>
2025-02-12 03:31:28 +0600

    companyKeyAdded in auth user

--- app/Http/Controllers/Api/PublisherOtpController.php
+++ app/Http/Controllers/Api/PublisherOtpController.php
@@ -378,7 +378,7 @@ class PublisherOtpController extends Controller
         $user = Auth::guard('api')->user();
         $data = $request->only(['asset_id','min_duration_in_hour','price_per_hour', 'zone_id']);
         $secureApiUser = SecureApi::getUser($user->secure_api_id);
-        $data['url'] = $request->url ? "{$request->url}?org_slug={$secureApiUser['companyKey']}": '';
+//        $data['url'] = $request->url ? "{$request->url}?org_slug={$secureApiUser['companyKey']}": '';
         $validator = $this->asrv->validateAsset($request->asset_id,$request->min_population,$request->max_population ?? null);
         $asset = $this->asr->find($data['asset_id']);
 
