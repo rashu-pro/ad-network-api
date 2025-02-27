1f01f368c69d397bd46e2d86e09ce4809523a5f4
ahamed-kashif <ahamed.kashif@northsouth.edu>
2025-02-27 18:40:00 +0600

    typo

--- app/Http/Controllers/Api/PublisherOtpController.php
+++ app/Http/Controllers/Api/PublisherOtpController.php
@@ -794,7 +794,7 @@ class PublisherOtpController extends Controller
         $campaign = Campaign::find($id);
         if(!$campaign){
             $request->validate([
-                'zone_id' => 'required|integer|exists:publisher_assets,zone_id',
+                'zoneId' => 'required|integer|exists:publisher_assets,zone_id',
                 'banner' => 'required|file|mimes:jpg,jpeg,png',
                 'companyKey' => 'required'
             ]);
