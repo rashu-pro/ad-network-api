756275098902c341b4fddf7e8af18749b3bd656f
ahamed-kashif <ahamed.kashif@northsouth.edu>
2025-02-27 18:34:56 +0600

    publisher campaign upload resolved

--- app/Http/Controllers/Api/PublisherOtpController.php
+++ app/Http/Controllers/Api/PublisherOtpController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Enums\CampaignStatus;
 use App\Enums\PaymentStatus;
 use App\Enums\PublisherCampaignStatus;
+use App\Enums\RolesEnum;
 use App\Exceptions\SecureApiException;
 use App\Facades\SecureApi;
 use App\Http\Controllers\Controller;
@@ -16,16 +17,22 @@ use App\Models\Asset;
 use App\Models\Campaign;
 use App\Models\CampaignMapping;
 use App\Models\PublisherAsset;
+use App\Models\User;
 use App\Models\Zone;
 use App\Repositories\Interfaces\AssetRepositoryInterface;
 use App\Repositories\Interfaces\AssetValuationRepositoryInterface;
 use App\Repositories\Interfaces\CampaignMappingRepositoryInterface;
 use App\Repositories\Interfaces\CampaignRepositoryInterface;
+use App\Services\AdvertiserService;
 use App\Traits\ApiResponse;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use function Symfony\Component\String\s;
 use OpenApi\Attributes as OA;
@@ -35,12 +42,19 @@ class PublisherOtpController extends Controller
     protected AssetRepositoryInterface $asr;
     protected AssetValuationRepositoryInterface $asrv;
     protected CampaignRepositoryInterface $cmp;
+    protected AdvertiserService $advertiserService;
 
-    public function __construct(AssetRepositoryInterface $asr, AssetValuationRepositoryInterface $asrv, CampaignRepositoryInterface $cmp)
+    public function __construct(
+        AssetRepositoryInterface $asr,
+        AssetValuationRepositoryInterface $asrv,
+        CampaignRepositoryInterface $cmp,
+        AdvertiserService $advertiserService
+    )
     {
         $this->asr = $asr;
         $this->asrv = $asrv;
         $this->cmp = $cmp;
+        $this->advertiserService = $advertiserService;
     }
 
     #[OA\Get(
@@ -774,5 +788,128 @@ class PublisherOtpController extends Controller
             }
         }
         return $this->errorResponse('No zone AdServer id found');
+    }
+
+    public function updateCampaign(Request $request, int $id = null){
+        $campaign = Campaign::find($id);
+        if(!$campaign){
+            $request->validate([
+                'zone_id' => 'required|integer|exists:publisher_assets,zone_id',
+                'banner' => 'required|file|mimes:jpg,jpeg,png',
+                'companyKey' => 'required'
+            ]);
+            $user = User::where('secure_api_id',$request->companyKey)->firstOrFail();
+
+            $campaignData = $request->only([
+                'campaign_name', 'target_url', 'start_date', 'end_date',
+            ]);
+            $campaignData['advertiser_id'] = $user->id;
+            $campaignData['publisher_id'] = $user->id;
+            $publisher_adserver_id = $user->publisher_advertiser_id;
+
+            //get publisher_advertiser_id
+            if(!$publisher_adserver_id){
+                // Payload data
+                $payload = [
+                    'advertiserName' => $user->name ?? 'test_advertiser_'.$user->id,
+                    'contactName'    => $user->name ?? 'test_advertiser_'.$user->id,
+                    'emailAddress'   => $user->email,
+                    'username'       => $user->email,
+                ];
+
+                // Send GET request with Basic Auth
+                $endpoint = env('AD_SERVER_BASE_URL').'/adv/new';
+                $response = Http::withBasicAuth(env('AD_SERVER_SUPER_ADMIN_USERNAME'), env('AD_SERVER_SUPER_ADMIN_PASSWORD'))->post($endpoint, $payload);
+                $advertiser_id = $response->object()->advertiserId;
+
+                //After the successful response add the advertiser_id into database
+                $user->publisher_adserver_id = $advertiser_id;
+                $publisher_adserver_id = $advertiser_id;
+                $user->save();
+                $user->refresh();
+            }
+
+            $campaignData['advertiser_adserver_id'] = $publisher_adserver_id;
+            $campaignData['status'] = CampaignStatus::PUBLISH;
+            //campaign create
+            $campaign = $this->advertiserService->createCampaign($campaignData);
+
+            //mapping create
+            $publishersData = User::whereHas('roles', function ($query) {
+                $query->where('name', RolesEnum::PUBLISHER->value);
+            })->where('id', $request->publisher_id)
+                ->has('assets')
+                ->with('assets')
+                ->get()
+                ->map(function ($publisher) use ($request,$user){
+                    return $publisher->assets->map(function ($asset) use ($publisher,$request,$user) {
+                        $startDate = Carbon::parse($request->start_date);
+                        $endDate = Carbon::parse($request->end_date);
+                        $days = $startDate->diffInDays($endDate) + 1; // Include the start day
+                        // Calculate the total price
+                        $calculatedPrice = $asset->price_per_hour * 24 * $days;
+
+                        return [
+                            'advertiser_id' => $user->id,
+                            'publisher_id' => $publisher->id,
+                            'publisher_asset_id' => $asset->id,
+                            'publisher_zone_id' => $asset->zone_id,
+                            'start_date' => $request->start_date,
+                            'end_date' => $request->end_date,
+                            'status' => PublisherCampaignStatus::APPROVE,
+                            'calculated_price' => 0,
+                            'is_active' => true,
+                        ];
+                    })->toArray(); // Convert collection to array
+                })
+                ->flatten(1) // Flatten nested arrays
+                ->toArray(); // Convert to a plain array
+            $this->advertiserService->selectPublishers($campaign->id,$publishersData);
+        }
+
+        //updating banner
+        $mappings = $campaign->mappings()->where('publisher_id',$request->publisher_id)
+            ->where('publisher_zone_id',$request->zone_id)
+            ->get();
+
+        if($mappings->count() <= 0){
+            return $this->errorResponse('Provided zone or publisher is not associated with this campaign');
+        }
+
+        $zone = Zone::findOrFail($request->zone_id);
+        $validator = Validator::make($request->all(), [
+            'banner' => 'required|file|mimes:jpg,jpeg,png|dimensions:width=' . $zone->width . ',height=' . $zone->height,
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator,'Invalid data',$validator->errors());
+        }
+
+        $tempPath = $request->file('banner')->store('temp');
+        $bannerPath = storage_path('app/private/' . $tempPath);
+
+        foreach ($mappings as $mapping) {
+            if($mapping->hasMedia('banner')){
+                $mapping->clearMediaCollection('banner');
+            }
+            $mapping->addMedia($bannerPath)
+                ->withCustomProperties([
+                    'publisher_id' => $mapping->publisher_id,
+                    'publisher_zone_id' => $mapping->publisher_zone_id,
+                    'publisher_asset_id' => $mapping->publisher_asset_id,
+                    'campaign_id' => $campaign->id,
+                ])
+                ->preservingOriginal()
+                ->toMediaCollection('banner');
+        }
+        Storage::delete('app/private/'.$tempPath);
+        return $this->successResponse(message: 'uploaded successfully', data: [
+            'campaign_id' => $campaign->id,
+            'url' => $mappings->filter(function ($mapping){
+                return $mapping->hasMedia('banner');
+            })->map(function ($mapping) {
+                return  $mapping->getFirstMedia('banner')->getUrl();
+            })->toArray()
+        ]);
     }
 }
