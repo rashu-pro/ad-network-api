cd3a11973e07ecbaccc2652563495eef8dcbc0e9
ahamed-kashif <ahamed.kashif@northsouth.edu>
2025-05-29 19:34:06 +0600

    fixed bug

--- app/HttpModels/SecureApiUser.php
+++ app/HttpModels/SecureApiUser.php
@@ -25,7 +25,7 @@ class SecureApiUser implements HttpModel
     {
         // Base validation rules
         $rules = [
-            'email' => 'required',
+            'email' => 'required|email|max:255',
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'business_name' => 'required|string|max:255',
