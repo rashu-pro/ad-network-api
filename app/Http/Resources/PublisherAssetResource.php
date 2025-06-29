d31970b98c2a438dbb04ebba5347cc9a492f40b8
rashu-pro <rashu.pro@gmail.com>
2025-06-29 10:50:12 +0600

    null checked added in PublisherAssetResource

--- app/Http/Resources/PublisherAssetResource.php
+++ app/Http/Resources/PublisherAssetResource.php
@@ -16,8 +16,8 @@ class PublisherAssetResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'asset_name' => $this->asset->name,
-            'asset_type' => $this->asset->assetType->name,
+            'asset_name' => $this->asset?->name,
+            'asset_type' => $this->asset?->assetType?->name,
             'url' => $this->url,
             'min_duration_in_hour' => $this->min_duration_in_hour,
             'price_per_hour' => $this->price_per_hour,
