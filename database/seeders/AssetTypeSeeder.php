86246475cc1cdff00c5ba7ce4c369acd096b5934
rashu-pro <rashu.pro@gmail.com>
2025-07-05 01:25:44 +0600

    asset type and categories seeder updated

--- database/seeders/AssetTypeSeeder.php
+++ database/seeders/AssetTypeSeeder.php
@@ -14,13 +14,11 @@ class AssetTypeSeeder extends Seeder
     public function run(): void
     {
         AssetType::firstOrCreate(
-            ['name' => 'Online'],
-            ['description' => 'Online', 'status' => 1]
+            ['name' => 'Online', 'description' => 'Online', 'status' => 1]
         );
 
         AssetType::firstOrCreate(
-            ['name' => 'Offline'],
-            ['description' => 'Offline', 'status' => 1]
+            ['name' => 'Offline', 'description' => 'Offline', 'status' => 1],
         );
     }
 
