5e2516b90afc04c64569720569cb5a8ccffcedeb
asif <asif@techknowworld.com>
2025-07-08 09:37:14 +0600

    api modified

--- routes/api.php
+++ routes/api.php
@@ -108,7 +108,7 @@ Route::prefix('advertiser')->middleware('auth:api')->group(function (){
 Route::prefix('publisher')->middleware('auth:api')->group(function (){
     Route::get('/campaign/{campaign}',[\App\Http\Controllers\Api\AdvertiserOptController::class,'getCampaign']);
     Route::post('/set-asset',[\App\Http\Controllers\Api\PublisherOtpController::class,'setAsset'])->middleware('permission:create asset,api');
-    Route::put('/asset/update/{id}', [\App\Http\Controllers\Api\PublisherOtpController::class, 'updateAsset'])->middleware('permission:update asset,api');
+    Route::post('/asset/update/{id}', [\App\Http\Controllers\Api\PublisherOtpController::class, 'updateAsset'])->middleware('permission:update asset,api');
     Route::post('/campaign-approval/{campaign}',[\App\Http\Controllers\Api\PublisherOtpController::class,'publishCampaign'])->middleware('can:updateStatus,campaign');
     Route::post('/update-campaign-mapping-status/{campaignMapping}',[\App\Http\Controllers\Api\PublisherOtpController::class,'updateCampaignMappingStatus']);
     Route::get('/assets',[\App\Http\Controllers\Api\PublisherOtpController::class,'assets'])->middleware('role:ad_publisher,api');
